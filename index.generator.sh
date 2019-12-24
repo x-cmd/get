5774a35ae7866b8f6757062daac70107448c18c7
edwinjhlee <edwin.jh.lee@gmail.com>
2019-12-25 07:47:36 +0800

    provide index

--- index.generator.sh
+++ index.generator.sh
@@ -2,7 +2,15 @@ subfolder=(
     std net cmd cloud other
 )
 
+# std for standard library files
+# net for security cmds, like nmap, metasploit
+# cloud for provider
+# cmd for other command not belong above
+
+# We need to consider using the flat naming scheme
+
+rm index
 for i in "${subfolder[@]}"; do
-    echo "$(ls $i/* 2>/dev/null)" >>index
-done
+    ls $i/*
+done 2>/dev/null >>index
 
