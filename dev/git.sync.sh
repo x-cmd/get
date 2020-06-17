33255ecc46c81d554af08674e0c7b8c4bf587f14
edwinjhlee <edwin.jh.lee@gmail.com>
2020-06-17 21:32:43 +0800

    fix

--- dev/git.sync.sh
+++ dev/git.sync.sh
@@ -1,4 +1,4 @@
 git remote remove gitee 2>/dev/null
-git remote add gitee git@gitee.com:x-bash/cloud.git
+git remote add gitee git@gitee.com:x-bash/x-bash.gitee.io.git
 git push gitee
 git push origin
