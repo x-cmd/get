a8dcac9f35c2eeba4a1b14a3b21f27605a2ce87f
edwinjhlee <edwin.jh.lee@gmail.com>
2020-08-22 22:39:19 +0800

    sync to github and gitee

--- dev/git.sync.sh
+++ dev/git.sync.sh
@@ -1,4 +1,9 @@
 git remote remove gitee 2>/dev/null
 git remote add gitee git@gitee.com:x-bash/x-bash.git
 git push gitee
+
+git remote remove github 2>/dev/null
+git remote add github git@github.com:x-bash/x-bash.github.io.git
+git push github
+
 git push origin
