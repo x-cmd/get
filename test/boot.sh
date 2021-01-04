4736705af5dc675b60e05de9ea3fce69890e137c
edwinjhlee <edwin.jh.lee@gmail.com>
2021-01-04 20:29:07 +0800

    stash

--- test/boot.sh
+++ test/boot.sh
@@ -6,18 +6,21 @@ xrc std/assert
 xrc.which std/assert
 
 bash <<A
-command -v "assert" && echo "ERROR: EXPECT NOT contains assert"
+set -o errexit
 
+command -v "assert" && echo "ERROR: EXPECT NOT contains assert"
 xrc std/assert
+command -v "assert" || echo "ERROR: EXPECT assert module loading"
 
 # assert.file "$(xrc.which std/assert)"
 
 # assert.exists "$X_BASH_SRC_PATH/index"
 rm "$X_BASH_SRC_PATH/index"
+# assert.not.exists "$X_BASH_SRC_PATH/index"
 xrc.update
 # assert.exists "$X_BASH_SRC_PATH/index"
 A
 
-echo $?
+[ $? -ne 0 ] && echo "ERROR"
 
 rm -rf ./std
